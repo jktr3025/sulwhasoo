98a834fd11dbd344420e9b1be46e2c2e780cab7d
jktr3025 <jktr3025@naver.com>
2024-01-23 09:18:09 +0900

    sulwhasoo day_1

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(function(){
+    //language
+    $(".select_language").click(function(){
+        $(this).toggleClass("show");
+    })//
+
+    //menu
+    $(".sub").hide();
+    $(".gnb_menu > li").hover(function(){
+        $(this).find(".sub").stop().slideDown();
+        $(".gnb_menu").stop().animate({height:"80px"},500);
+    }, function(){
+        $(this).find(".sub").stop().slideUp();
+        $(".gnb_menu").stop().animate({height:"50px"},500);
+    })
+
+})//jquery
